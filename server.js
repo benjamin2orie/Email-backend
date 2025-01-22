5b0d1b7232e3def4e9252c1fbd0dbb4833df17e2
benjamin2orie <ogechukwubenj@gmail.com>
2025-01-22 13:20:32 +0100

    wrapt the connectionstring in async function

--- server.js
+++ server.js
@@ -10,14 +10,28 @@ const Dev = process.env.NODE_ENV;
 console.log(port);
 console.log(Dev);
 console.log('greetings from backend server');
-const Db = process.env.DB_URL
-const dbConnect = await mongoose.connect(Db).then(() =>{
-    console.log('Db connected succesfully')
-})
-dbConnect()
-.catch((err) =>{
-    console.log('Unable to connect to db', err)
-});
+
+
+// const Db = process.env.DB_URL
+// const dbConnect = await mongoose.connect(Db).then(() =>{
+//     console.log('Db connected succesfully')
+// })
+// dbConnect()
+// .catch((err) =>{
+//     console.log('Unable to connect to db', err)
+// });
+
+const Db = process.env.DB_URL;
+ const connectToDb = async () => {
+     try {
+         await mongoose.connect(Db || ""); 
+            console.log('Db connected successfully');
+         } catch (err) {
+             console.log('Unable to connect to db', err);
+             } };
+
+
 app.listen(port, ()=>{
     console.log(`The server started running on port ${port}`)
+    connectToDb()
 });
