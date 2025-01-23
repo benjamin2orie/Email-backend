9dcc21a85df72b618275c76c2fceeb81fb28e73b
benjamin2orie <ogechukwubenj@gmail.com>
2025-01-23 16:03:13 +0100

    modified controllers file

--- Controller/authController.js
+++ Controller/authController.js
@@ -18,7 +18,7 @@ const createSendToken = (user, statusCode, res, message) =>{
     const cookieOptions = {
         expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
+        secure: process.env.NODE_ENV === "production" || true,
         sameSite:process.env.NODE_ENV === "production" ? "none" : "lax"
     };
 
