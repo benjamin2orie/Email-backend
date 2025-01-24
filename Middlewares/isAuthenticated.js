4fd5019907b99b9c62825ef7e817591e47ff9d5a
benjamin2orie <ogechukwubenj@gmail.com>
2025-01-24 17:57:20 +0100

    added env to middleware

--- Middlewares/isAuthenticated.js
+++ Middlewares/isAuthenticated.js
@@ -3,6 +3,8 @@ import jwt from 'jsonwebtoken';
 import catchAsync from '../Utils/catchAsync.js';
 import AppError from '../Utils/appError.js';
 import User from '../Model/userModel.js';
+// import dotenv from 'dotenv';
+// dotenv.config();
 
 const isAuthenticated = catchAsync(async(req, res, next) =>{
 
@@ -10,8 +12,9 @@ const isAuthenticated = catchAsync(async(req, res, next) =>{
     const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
     if(!token){
         return next(new AppError('You are not logged in. Please log in to access', 401));
+        console.log(process.env.JWT_SECRETE)
     }
-
+     console.log(process.env.JWT_SECRETE)
     const decoded = jwt.verify(token, process.env.JWT_SECRETE);
     const currentUser = await User.findById(decoded.id);
 
