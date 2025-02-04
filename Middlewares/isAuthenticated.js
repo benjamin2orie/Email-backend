2bfeb88a3d54e71ec37315b54056755ccecd1afe
benjamin2orie <ogechukwubenj@gmail.com>
2025-02-04 11:02:52 +0100

    updated middleware

--- Middlewares/isAuthenticated.js
+++ Middlewares/isAuthenticated.js
@@ -10,7 +10,7 @@ const isAuthenticated = catchAsync(async(req, res, next) =>{
 
     const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
     if(!token){
-        return next(new AppError('You are not logged in. Please log in to access', 401));
+        return next(new AppError('This account does not exist😏', 401));
     }
     const decoded = jwt.verify(token, process.env.JWT_SECRETE);
     const currentUser = await User.findById(decoded.id);
